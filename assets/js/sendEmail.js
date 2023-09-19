549680f4c8f5838a3148718dc92f48b8d6d77534
Huddy2022 <mlvnhud@aol.com>
2023-09-19 19:19:33 +0000

    amend service id for email funcationality

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,16 +1,20 @@
 function sendMail(contactForm) {
-    emailjs.send("service_7z1vi7w", "Adam", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
+    emailjs.send("service_6juaw1f", "Adam", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "project_request": contactForm.project_request.value
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                contactForm.name.value = '';
+                contactForm.emailaddress.value = '';
+                contactForm.project_request.value = '';
+                alert("Success! Your email has been sent.");
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false;
 }
